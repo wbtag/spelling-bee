14f73f496b10d93ce3c01f1dfdbbd1b30b5440ad
Vojtěch Greger <vojtech@vbtag.cz>
2026-09-01 00:20:57 +0200

    feat: add czech version

---
+++ src/components/Acknowledgments.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+</script>
+<template>
+  <span>{{$t("AcknowledgementsText")}}</span>
+      <el-link
+        type="primary"
+        href="https://github.com/ConorSheehan1/spelling-bee"
+        target="_blank">
+        GitHub
+        <img
+          src="../assets/github.svg"
+          alt="github"
+          class="github-icon" />
+      </el-link>
+</template>
+
+<style scoped lang="scss">
+html.dark {
+  .github-icon {
+    filter: invert(0.8);
+  }
+}
+</style>
